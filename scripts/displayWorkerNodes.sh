5379cbbaedde75d4466ab25fbc32e15ff2580b9b
Eduardo Patrocinio <epatro@gmail.com>
2018-02-19 11:12:05 -0800

    Create displayWorkerNodes.sh

---
+++ scripts/displayWorkerNodes.sh
@@ -0,0 +1,5 @@
+echo Retrieving worker nodes...
+
+kubectl get no -o json | \
+		jq '.items[].metadata | select (.labels.role != "master") | select (.labels.proxy != "true") | .name' | \
+		tr -d '"' | tr -d '\n'
