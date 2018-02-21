22a849774d5216efba55ad1af830c58d460ca4af
Eduardo Patrocinio <epatro@gmail.com>
2018-02-21 06:27:50 -0800

    Create Docker image

---
+++ src/worker_node_utilization/scripts/checkUtilization.sh
@@ -0,0 +1,5 @@
+bx pr login -a https://10.0.0.1:8443 --skip-ssl-validation -u admin -p admin
+bx pr cluster-config mycluster
+
+python3 top_worker_nodes.py
+
--- src/worker_node_utilization/src/checkUtilization.sh
+++
@@ -1,2 +0,0 @@
-bx pr login -a https://10.0.0.1:8443 --skip-ssl-validation -u admin -p admin
-bx pr cluster-config mycluster
