05a7eff1af447606d02a170b624ebf14e2087a54
Eduardo Patrocinio <epatro@gmail.com>
2018-02-21 07:05:00 -0800

    Set ICP instance in bx pr login

--- src/worker_node_utilization/scripts/checkUtilization.sh
+++ src/worker_node_utilization/scripts/checkUtilization.sh
@@ -1,4 +1,4 @@
-bx pr login -a https://10.0.0.1:8443 --skip-ssl-validation -u admin -p admin
+bx pr login -a https://10.0.0.1:8443 --skip-ssl-validation -u admin -p admin -c id-icp-account
 bx pr cluster-config mycluster
 
 python3 top_worker_nodes.py
