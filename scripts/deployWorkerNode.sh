f71c1c09fcc42d6b3ace6f966207816e830b8249
Eduardo Patrocinio <epatro@gmail.com>
2018-02-22 10:28:52 -0800

    Troubleshoot deployWorkerNode

--- scripts/deployWorkerNode.sh
+++ scripts/deployWorkerNode.sh
@@ -2,7 +2,7 @@ echo Deleting deployWorkerNode job
 kubectl delete job deploy-worker-node
 
 echo Deploying a worker node
-echo Node: $1
+echo Worker node: $1
 export WORKER=$1
 
 CONFIG_MAP=worker-node
